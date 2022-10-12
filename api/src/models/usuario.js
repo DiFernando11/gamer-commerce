d4c3cfec3e14ef251e8f8249d126953f3d6332ee
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-10-12 17:30:52 -0300

    Update: user model complete

--- api/src/models/usuario.js
+++ api/src/models/usuario.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-    // defino el modelo
     sequelize.define('user', {
       name: {
         type: DataTypes.STRING,
