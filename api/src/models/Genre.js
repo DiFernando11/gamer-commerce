6ae2f8bbac19bc954ea4adacea2b9b235af518b1
Roger Perez <roger.perezcol@gmail.com>
2022-10-13 13:41:33 -0300

    Db genres Loaded

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -4,7 +4,11 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('genre', {
-   
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
