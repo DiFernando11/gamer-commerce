28ca72bd79844f6f09720520a4307617a39ba15b
DiFernando11 <102831050+DiFernando11@users.noreply.github.com>
2022-10-13 18:08:14 -0500

    feat/detail game create

---
+++ client/src/components/detailGame/index.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import styles from "./index.module.css";
+
+function DetailGame() {
+  const videoGames = {
+    name: "Apex Lengend",
+    imgMain:
+      "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/a/apex-legends-switch/hero",
+    img: [
+      "https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop16x9.1023w.jpg",
+      "https://as01.epimg.net/meristation/imagenes/2022/03/09/mexico/1646788340_221821_1646788475_noticia_normal.jpg",
+      "https://play-lh.googleusercontent.com/-d8ESMQOyN6BdCH5TQFUKTcsg1C7nbL2E7fM3RioHmJJWtYxdNDXdc-GAjaHvfKT3mU=w526-h296-rw",
+      "https://media.contentapi.ea.com/content/dam/apex-legends/mobile/images/2022/05/primtimetimeblogheader-1.png.adapt.crop191x100.628p.png",
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/05/apex-legends-2325601.jpg?itok=1Bp2eKkQ",
+    ],
+  };
+  const [imageCurrent, setImageCurrent] = useState(videoGames.img);
+  const [commentUser, setCommentUser] = useState("");
+  const [allComments, setAllComments] = useState([]);
+  const hanldeImage = (value) => {
+    setImageCurrent(value);
+  };
+  console.log(commentUser);
+  let handleChange = (e) => {
+    e.preventDefault();
+    setCommentUser(e.target.value);
+  };
+  const handleViewComment = (comment) => {
+    if (commentUser.length) {
+      setAllComments([...allComments, comment]);
+    }
+    setCommentUser("");
+  };
+  console.log(allComments);
+
+  return (
+    <section>
+      <div className={styles.sectionDetailGame}>
+        <div>
+          <div className={styles.containerImageMainGame}>
+            <h1>{videoGames.name} </h1>
+            <img
+              className={styles.imgMainGame}
+              src={imageCurrent}
+              alt="logo main game"
+            />
+            <ul className={styles.container_images_secondary}>
+              {videoGames.img.length
+                ? videoGames.img.map((game) => (
+                    <li onClick={() => hanldeImage(game)}>
+                      <img src={game} alt="logo" />
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </div>
+          <p className={styles.text_warning}>
+            Inicia sesión para añadir este artículo a tu lista de deseados,
+            seguirlo o marcarlo como ignorado.
+          </p>
+        </div>
+        <div className={styles.containerComment}>
+          <div className={styles.comment_user}>
+            <i className="bi bi-person-circle"></i>
+
+            <input
+              type="text"
+              name="comment"
+              placeholder="Danos tu opinion"
+              value={commentUser}
+              onChange={handleChange}
+              autoComplete="off"
+            />
+
+            <i
+              className="bi bi-send-check-fill"
+              onClick={() => handleViewComment(commentUser)}
+            ></i>
+          </div>
+          <div>
+            {allComments.length
+              ? allComments.map((comment, index) => (
+                  <div className={styles.container_comments_users}>
+                    <i className="bi bi-person-circle"></i>
+                    <p key={index}>{comment}</p>
+                  </div>
+                ))
+              : null}
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Comment</h1>
+      </div>
+    </section>
+  );
+}
+
+export default DetailGame;
--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -3,6 +3,7 @@ import CarouselGenres from "../carouselGenres";
 import CarrouselPunctuation from "../carouselPunctuation";
 import CarrouselRecommended from "../carouselRecommended";
 import CarosuelSectionPrice from "../carouselSectionPrice";
+import Footer from "../footer";
 
 function Home() {
   return (
