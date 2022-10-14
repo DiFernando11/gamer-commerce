a0f90540094d776d30d9793d7c6f2650244cac49
Andres Aldao <andresaldao9575@gmail.com>
2022-10-13 22:10:45 -0300

    navbar base

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -3,10 +3,12 @@ import CarouselGenres from "../carouselGenres";
 import CarrouselPunctuation from "../carouselPunctuation";
 import CarrouselRecommended from "../carouselRecommended";
 import CarosuelSectionPrice from "../carouselSectionPrice";
+import NavBar from "../nav-bar";
 
 function Home() {
   return (
     <main>
+      <NavBar />
       <CarrouselRecommended />
       <CarosuelSectionPrice />
       <CarouselGenres />
