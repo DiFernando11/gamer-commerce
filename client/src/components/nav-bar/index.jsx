350acb089934b05d5319eca60f4b3c99823a5be4
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-10-17 12:57:49 -0300

    problem with search bar version fixed

--- client/src/components/nav-bar/index.jsx
+++ client/src/components/nav-bar/index.jsx
@@ -38,15 +38,6 @@ const NavBar = () => {
                   
                     </li>
                     <li className="nav-item dropdown border-0 select-dark" >
-           
-                   {/*  <Link className="nav-link text-light dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Categories
-                    </Link> */}
-                 {/*    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul> */}
               </li>
             </ul>
           </div>
