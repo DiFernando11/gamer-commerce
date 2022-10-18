46a95f6db1f767975e43b4aefe46ad7f3775fe4c
Andres Aldao <andresaldao9575@gmail.com>
2022-10-18 20:53:20 -0300

    sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,13 @@ import Footer from "./components/footer";
 import NavBar from "./components/nav-bar";
 import Genres from "./components/genres";
 import YourCart from "./components/yourCart";
-// import adminHome from "./components/Dashboard/adminhome";
-// import adminlogin from "./components/Dashboard/adminlogin";
-// import adminorders from "./components/Dashboard/adminorders";
-// import admingames from "./components/Dashboard/admingames";
-// import adminUsers from "./components/Dashboard/adminusers";
-// import adminuser from "./components/Dashboard/adminuser";
-// import adminGame from "./components/Dashboard/admingame";
+import adminHome from "./components/Dashboard/adminhome";
+import adminlogin from "./components/Dashboard/adminlogin";
+import adminorders from "./components/Dashboard/adminorders";
+
+import adminUsers from "./components/Dashboard/adminusers";
+import adminuser from "./components/Dashboard/adminuser";
+import adminGame from "./components/Dashboard/admingame";
 
 
 function App() {
@@ -50,14 +50,13 @@ function App() {
       />
 
 
-    {/*   <Route exact path={"/admin"} component={adminHome} />
-      <Route exact path={"/admin/login"} component={adminlogin} />
+      <Route exact path={"/admin"} component={adminHome} />
+{/*       <Route exact path={"/admin/login"} component={adminlogin} />
       <Route exact path={"/admin/users"} component={adminUsers} />
       <Route exact path={"/admin/users/:userid"} component={adminuser} />
       <Route exact path={"/admin/orders"} component={adminorders} />
-      <Route exact path={"/admin/games"} component={admingames} />
-      <Route exact path={"/admin/games/:gameid"} component={adminGame} /> */}
-
+      <Route exact path={"/admin/games/:gameid"} component={adminGame} />
+ */}
     </>
   );
 }
--- client/src/components/Dashboard/side-bar/index.jsx
+++ client/src/components/Dashboard/side-bar/index.jsx
@@ -1,30 +1,54 @@
 import React from "react";
 import "./index.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import GamesIcon from '@mui/icons-material/Games';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const SideBar = () => {
     return (
         <div className="sidebar">
-            <div className="top">logo</div>
+            <div className="top">
+                <span className="logo">logo</span>
+            </div>
+            <hr className="hr"/>
             <div className="center">
                 <ul>
+                    <p className="titlesidebar">MAIN</p>
                     <li>
+                        <DashboardIcon className="iconsidebar"/>
                         <span>Dashboard</span>
                     </li>
+                    <p className="titlesidebar">LIST</p>
                     <li>
-                        <span>Dashboard</span>
+                        <PeopleAltIcon className="iconsidebar"/>
+                        <span>Users</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <GamesIcon className="iconsidebar"/>
+                        <span>Games</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <ShoppingCartIcon className="iconsidebar"/>
+                        <span>Orders</span>
+                    </li>
+                    <p className="titlesidebar">USER</p>
+                    <li>
+                        <AccountBoxIcon className="iconsidebar"/>
+                        <span>Profile</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <LogoutIcon className="iconsidebar"/>
+                        <span>Logout</span>
                     </li>
                 </ul>
             </div>
-            <div className="bottom">color options</div>
+            <div className="bottom1">
+                <div className="coloroption"></div>
+                <div className="coloroption"></div>
+            </div>
         </div>
             
     );
