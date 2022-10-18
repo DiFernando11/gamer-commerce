f167be315394db92e2878b97801e6236f21ca3ee
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-10-18 18:51:51 -0300

    cambio de nombre del modelo usuario, ruta detalle del usuario

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,30 +2,26 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
-const {validator}= require("./middleware/validator")
+const { validator } = require('./middleware/validator');
 
-const search = require('./search')
-const detail = require('./gameDetail')
-const genre = require('./genre')
-const filtered = require('./filtered')
-const create = require('./createGame')
-const {singIn,singUp} = require('./auth')
-
+const search = require('./search');
+const detail = require('./gameDetail');
+const genre = require('./genre');
+const filtered = require('./filtered');
+const create = require('./createGame');
+const { singIn, singUp } = require('./auth');
+const user = require('./userDetail');
 
 router.use('/search', search);
 router.use('/detail', detail);
 router.use('/genre', genre);
-router.use('/filtered', filtered)
-router.use('/creategame', create)
+router.use('/filtered', filtered);
+router.use('/creategame', create);
 router.post('/signin', singIn);
 router.post('/signup', singUp);
-
-
-
+router.use('/user', user);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 module.exports = router;
-
-
