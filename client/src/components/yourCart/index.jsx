e4483fb658c6b7e5c1a3f6c9916dc3305d5beb41
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-10-22 19:51:28 -0300

    update ux 3.0

--- client/src/components/yourCart/index.jsx
+++ client/src/components/yourCart/index.jsx
@@ -56,7 +56,7 @@ function YourCart() {
                 onClick={handleDeleteAllLocalStorage}
                 className={styles.deleteAllProducts}
               >
-                Eliminar todos los articulos
+                Remove all items
               </span>
             </div>
           </div>
@@ -72,9 +72,9 @@ function YourCart() {
               <span className={styles.discount}>-50%</span>
               <div className={styles.priceCurrentText}>
                 <span>$24.99</span>
-                <span>$12.49 USD</span>
+                <span>USD $12.49</span>
               </div>
-              <span className={styles.textOfertsDailys}>!oferta del dia!</span>
+              <span className={styles.textOfertsDailys}>!Deal of the day!</span>
             </div>
           </div>
           <div className={styles.containerCardOffers}>
@@ -88,7 +88,7 @@ function YourCart() {
                 <span>$24.99</span>
                 <span>$12.49 USD</span>
               </div>
-              <span className={styles.textOfertsDailys}>!oferta del dia!</span>
+              <span className={styles.textOfertsDailys}>!Deal of the day!</span>
             </div>
           </div>
         </div>
