348a0cd0b15e89ca562f1edd160fee3a1782adba
DiFernando11 <102831050+DiFernando11@users.noreply.github.com>
2022-10-26 18:24:03 -0500

    feat/progressorder

--- client/src/components/Dashboard/gameDashboard/index.jsx
+++ client/src/components/Dashboard/gameDashboard/index.jsx
@@ -1,26 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./index.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteGame, getAllGames } from "../../../redux/actions";
 import { Link } from "react-router-dom";
 
-
 function GameDashBoard() {
   const allGames = useSelector((state) => state.allGames);
+  const [viewElements, setViewElements] = useState(1);
+  const [orderAmount, setOrderAmount] = useState("All");
   let dispatch = useDispatch();
   const [active, setActive] = React.useState(true);
+
+  let postsPerPage = 20;
+  const lastPostIndex = viewElements * postsPerPage; // 4 //8
+  const currentPosts = allGames?.slice(0, lastPostIndex);
+  console.log(currentPosts, "games");
+
+  const deletegame = (id, banned) => {
+    dispatch(deleteGame(id, banned));
+    setActive(!active);
+  };
+  const handleOrderAmount = (order, idCheckbox, e) => {
+    // var isChecked = document.getElementById(idCheckbox).checked;
+    // if (isChecked) document.getElementById(idCheckbox).checked = false;
+    setOrderAmount(e.target.value);
+  };
 
   useEffect(() => {
     return () => dispatch(getAllGames());
   }, [dispatch]);
 
-  const deletegame = (id , banned) => {
-    dispatch(deleteGame(id, banned));
-    setActive(!active);
-  };
-
   return (
     <section className={styles.mainGamesAllDashboard}>
+      <div class="custom-control custom-switch">
+        <label class="custom-control-label" for="customSwitch1">
+          Toggle this switch element
+        </label>
+      </div>
+      <input
+        id="idOrderAmountCheck"
+        type={"checkbox"}
+        name="orderAmount"
+        value={"MAYOR"}
+        checked={orderAmount === "All" ? true : false}
+        onChange={(e) => handleOrderAmount("MAYOR", "idOrderAmountCheck", e)}
+      />
+      <input
+        id="idOrderAmountCheckLower"
+        type={"checkbox"}
+        name="orderAmount"
+        value={"MENOR"}
+        checked={orderAmount === "All" ? true : false}
+        onChange={(e) =>
+          handleOrderAmount("MENOR", "idOrderAmountCheckLower", e)
+        }
+      />
       <table className={styles.tableGames}>
         <tbody>
           <tr className={styles.tableTitles}>
@@ -31,37 +65,46 @@ function GameDashBoard() {
             <th>Status</th>
             <th>Action</th>
           </tr>
-
-          {allGames.length
-            ? allGames.map((game, index) => (
-                <tr key={index} className={styles.tableColumns}>
-                  <td className={styles.columnIdGame}>{game.id}</td>
-                  <td className={styles.columnNameGame}>
-                    <Link
-                      style={{ textDecoration: "none" }}
-                      to={`/admin/games/detail/${game.id}`}
-                    >
-                      <img src={game.image} alt={game.name} />
-                      <span>{game.name}</span>
-                    </Link>
-                  </td>
-                  <td className={styles.columnPriceGame}>${game.price}</td>
-                  <td className={styles.columnRatingGame}>{game.rating}</td>
-                  <td className={styles.columnStatusGame}>{game.show === true ? "Active" : "No Active"}</td>
-                  <td className={styles.columnActionGame}>
-                    <div>
-                      <span className={styles.columnActionView}>View</span>
-                      <button className={styles.columnActionDelete}
+          {currentPosts?.length &&
+            currentPosts.map((game, index) => (
+              <tr key={index} className={styles.tableColumns}>
+                <td className={styles.columnIdGame}>{game.id}</td>
+                <td className={styles.columnNameGame}>
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={`/admin/games/detail/${game.id}`}
+                  >
+                    <img src={game.image} alt={game.name} />
+                    <span>{game.name}</span>
+                  </Link>
+                </td>
+                <td className={styles.columnPriceGame}>${game.price}</td>
+                <td className={styles.columnRatingGame}>{game.rating}</td>
+                <td className={styles.columnStatusGame}>
+                  {game.show === true ? "Active" : "No Active"}
+                </td>
+                <td className={styles.columnActionGame}>
+                  <div>
+                    <span className={styles.columnActionView}>View</span>
+                    <button
+                      className={styles.columnActionDelete}
                       type="submit"
                       onClick={() => deletegame(game.id, game.show)}
-                      >Delete</button>
-                    </div>
-                  </td>
-                </tr>
-              ))
-            : null}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <span
+        className={styles.seeMore}
+        onClick={() => setViewElements(viewElements + 1)}
+      >
+        Ver mas
+      </span>
     </section>
   );
 }
