ce5af0e91e0b0550912604201fdbf9643bc30042
Luis <luismanlaz@hotmail..com>
2022-10-27 14:52:42 -0300

    gameplay

--- client/src/components/liveVideoGame/index.jsx
+++ client/src/components/liveVideoGame/index.jsx
@@ -5,6 +5,45 @@ function LiveVideoGame() {
   return (
     <section className={styles.sectionVideoGame}>
       <div className={styles.containerLiveVideoGame}>
+{/* 
+        <iframe
+          className={styles.videosGames}
+          src="https://www.youtube.com/embed/_CZn-05bc88?autoplay=1&mute=1&enablejsapi=1"
+          title="YouTube video player"
+        />
+
+        <iframe
+          className={styles.videosGames}
+          src="https://www.youtube.com/embed/wmNt4Rpc3HU?autoplay=1&mute=1&enablejsapi=1"
+          title="YouTube video player"
+        />
+
+        <iframe
+          className={styles.videosGames}
+          src="https://www.youtube.com/embed/p4QG59y6FGE?autoplay=1&mute=1&enablejsapi=1"
+          title="YouTube video player"
+        />
+
+        <iframe
+          className={styles.videosGames}
+          src="https://www.youtube.com/embed/W0gea2jLRdY?autoplay=1&mute=1&enablejsapi=1"
+          title="YouTube video player"
+        />
+
+        <iframe
+          className={styles.videosGames}
+          src="https://www.youtube.com/embed/s8G8jacm6Mw?autoplay=1&mute=1&enablejsapi=1"
+          title="YouTube video player"
+        />
+
+        <iframe
+          className={styles.videosGames}
+          src="https://www.youtube.com/embed/ba9cx77KziY?autoplay=1&mute=1&enablejsapi=1"
+          title="YouTube video player"
+        /> */}
+
+
+
         <img
           className={styles.videosGames}
           src="https://i.ytimg.com/vi/MNC6KNdCp58/maxresdefault.jpg"
@@ -36,25 +75,6 @@ function LiveVideoGame() {
           alt=""
         />
       </div>
-
-      {/* <video src="">
-          <source />
-      </video>
-      <video src="">
-          <source />
-      </video>
-      <video src="">
-          <source />
-      </video>
-      <video src="">
-          <source />
-      </video>
-      <video src="">
-          <source />
-      </video>
-      <video src="">
-          <source />
-      </video> */}
     </section>
   );
 }
