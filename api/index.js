6b5c4b81eceade6f2259293d9d3e4993ceb4b191
Roger Perez <roger.perezcol@gmail.com>
2022-10-30 18:02:17 -0300

    reviews

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const server = require('./src/app.js');
-const { conn, Game, Genre,Comment } = require('./src/db.js');
+const { conn, Game, Genre,Comment, Review } = require('./src/db.js');
 const {dataBaseLoader,genreLoader}=require("./src/utils/dbloader")
 const { PORT } = process.env;
 const PUERTO= PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(async() => {
-
-server.listen(PUERTO, () => {
+  Review.sync({ force: true })
+    server.listen(PUERTO, () => {
   console.log(`%s listening at ${PUERTO} ` ); // eslint-disable-line no-console
 });
 Genre.count().then(el=>{ 
