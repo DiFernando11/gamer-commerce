96358f1676ddebcf9165b42e48b70d3882a1d70f
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-11-10 14:56:21 -0300

    marge to main

--- client/src/components/carouselGenres/index.jsx
+++ client/src/components/carouselGenres/index.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import CarouselButtons from "../carouselButtons";
-import {useDispatch, useSelector} from "react-redux";
-import { useEffect} from "react";
-import { getGenres } from "../../redux/actions";
+import React from 'react';
+import CarouselButtons from '../carouselButtons';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getGenres } from '../../redux/actions';
 
 function CarouselGenres() {
-  const dispatch = useDispatch();
-  const genre = useSelector((state) => state.Genre);
- 
-  useEffect(() => {
-    dispatch(getGenres())
-  }, [dispatch]);
+	const dispatch = useDispatch();
+	const genre = useSelector((state) => state.Genre);
 
-  return (
-    <section id="Genres">
-      <CarouselButtons game={genre} category={true} />
-    </section>
-  );
+	useEffect(() => {
+		dispatch(getGenres());
+	}, [dispatch]);
+
+	return (
+		<section id="Genres">
+			<CarouselButtons game={genre} category={true} />
+		</section>
+	);
 }
 
 export default CarouselGenres;
