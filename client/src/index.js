f953fd04709329b18ae61e4a1472ab6f15c31040
DiFernando11 <102831050+DiFernando11@users.noreply.github.com>
2022-11-14 11:35:39 -0500

    released/finisehd

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
  //axios.defaults.baseURL = "https://gamer-api.up.railway.app";
 
+ 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
